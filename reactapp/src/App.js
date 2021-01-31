c5ac60d0a49a6aa283b36502830932a80ef2e9b4
bkrajinovic <bkrajinovic@gmail.com>
2021-01-31 18:38:51 +0100

    Add scheduler

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import Fetch from './components/Fetch'
+import Fetch from './components/Fetch';
+import Scheduler from './components/Scheduler'
 
 
 function App() {
 
   return (
     <div className="App">
-      <Fetch />
+      {/* <Fetch /> */}
+      <Scheduler />
     </div>
   );
 }
